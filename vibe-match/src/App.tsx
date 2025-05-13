61c4b5b6bac5b277add8b8ed13feda8a848d63ab
Lotanna <tannaloamobi@gmail.com>
2025-05-13 20:53:57 +0100

    added more classNames and styled page

--- vibe-match/src/App.tsx
+++ vibe-match/src/App.tsx
@@ -4,42 +4,46 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <header className='page-header'>
-        <h1 id='brandName'>🎶 Vibe Match</h1>
-      </header>
-      <main className="">
-        <input className="search-bar" type="text" />
-        {/* <button>
-          onClick={() =>}
-        </button> */}
-      <table className='song-table'>
-            <tr className='table-headings'>
-                <th>#</th>
-                <th>SONG TITLE</th>
-                <th>ARTIST</th>
-            </tr>
-            <tr className=""></tr>
-            <tr className=""></tr>
-            <tr className=""></tr>
-            <tr className=""></tr>
-            <tr className=""></tr>
-            <tr className=""></tr>
-            <tr className=""></tr>
-            <tr className=""></tr>
-            <tr className=""></tr>
-            <tr className=""></tr>
-        </table>
-      </main>
-      <aside className='side-menu'>
+    <div className='app-container'>
+      <aside className="side-menu">
         <a href="">Genre</a>
         <a href="">Tempo</a>
         <a href="">Danceable</a>
         <a href="">Time period</a>
         <a href=""></a>
       </aside>
-          
-    </>
+      <div className='main-content'>
+        <header className="page-header">
+          <h1 id="brandName">🎶 Vibe Match</h1>
+        </header>
+        <main className="">
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="What's the vibe?"
+          />
+          {/* <button>
+          onClick={() =>}
+        </button> */}
+          <table className="song-table">
+            <thead>
+              <tr className="table-headings">
+                <th>#</th>
+                <th>SONG TITLE</th>
+                <th>ARTIST</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="">
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </main>
+      </div>
+    </div>
   );
 }
 
