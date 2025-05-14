5c9d5bc2910654a52e9176e30d17a6d613f7dd7d
LizzieH97 <erhughes97@gmail.com#>
2025-05-14 10:29:24 +0100

    added router elements and added css to the side bar

--- vibe-match/src/App.tsx
+++ vibe-match/src/App.tsx
@@ -1,50 +1,20 @@
-
-import './App.css'
-
-function App() {
-
+import "./App.css";
+import SideBar from "./components/SideBar";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+export default function App() {
   return (
-    <div className='app-container'>
-      <aside className="side-menu">
-        <a href="">Genre</a>
-        <a href="">Tempo</a>
-        <a href="">Danceable</a>
-        <a href="">Time period</a>
-        <a href=""></a>
-      </aside>
-      <div className='main-content'>
-        <header className="page-header">
-          <h1 id="brandName">🎶 Vibe Match</h1>
-        </header>
-        <main className="">
-          <input
-            className="search-bar"
-            type="text"
-            placeholder="What's the vibe?"
-          />
-          {/* <button>
-          onClick={() =>}
-        </button> */}
-          <table className="song-table">
-            <thead>
-              <tr className="table-headings">
-                <th>#</th>
-                <th>SONG TITLE</th>
-                <th>ARTIST</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="">
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-            </tbody>
-          </table>
-        </main>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+const rootElement = document.getElementById("root");
 
-export default App
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+}
---
+++ vibe-match/src/components/SideBar.tsx
@@ -0,0 +1,26 @@
+export default function SideBar() {
+  return (
+    <>
+      <aside className="side-menu">
+        <p className="side-menu-intro">
+          Not sure what song to pick? Match your mood below:{" "}
+        </p>
+        <a href="" className="side-menu-button">
+          Choose your genre here
+        </a>
+        <a href="" className="side-menu-button">
+          Choose your tempo here
+        </a>
+        <a href="" className="side-menu-button">
+          Want a Dance? Or not? Choose here
+        </a>
+        <a href="" className="side-menu-button">
+          Choose your era here
+        </a>
+        <a href="" className="side-menu-button">
+          Take a look at all of your options here
+        </a>
+      </aside>
+    </>
+  );
+}
