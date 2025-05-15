5cfd8ddd7a5ad98f38daba07bcdc34b8c9c4a9d4
LizzieH97 <erhughes97@gmail.com#>
2025-05-15 14:34:04 +0100

    more merge conflict resolutions

--- vibe-match/src/components/Choices.tsx
+++ vibe-match/src/components/Choices.tsx
@@ -10,7 +10,6 @@ type ChoiceProps = {
   };
 };
 
-<<<<<<< HEAD
 export default function Choices({ choice }: ChoiceProps) {
   if (choice === ChoiceData.genre) {
     choice.buttons = getAllGenres();
@@ -37,28 +36,3 @@ export default function Choices({ choice }: ChoiceProps) {
     </>
   );
 }
-=======
-const Choices = ({ choice }: ChoiceProps) => {
-    return (
-            <div className="app-container">
-                <SideBar />
-                <div className="main-content">
-                    <Header />
-                    <main className="choice-grid">
-                        <h2 className="subtitle">{choice.subtitle}</h2>
-                        <a href="/" id="back-button">
-                            Want to go back to songs?
-                        </a>
-                        {choice.buttons.map((btn, idex) => (
-                            <button key={idex} className="choice-button">
-                                {btn}
-                            </button>
-                        ))}
-                    </main>
-                </div>
-            </div>
-    );
-};
-
-export default Choices;
->>>>>>> 8b489b810d48c687a294678b661f4e2a0fbae8e7
--- vibe-match/src/pages/Home.tsx
+++ vibe-match/src/pages/Home.tsx
@@ -19,30 +19,7 @@ const Home = () => {
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
-<<<<<<< HEAD
-
-            {/* <button>
-          onClick={() =>}
-        </button> */}
-            <table className="song-table">
-              <thead>
-                <tr className="table-headings">
-                  <th>#</th>
-                  <th>SONG TITLE</th>
-                  <th>ARTIST</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className="table-rows">
-                  <td className="song-numbers"></td>
-                  <td className="song-title"></td>
-                  <td className="song-artist"></td>
-                </tr>
-              </tbody>
-            </table>
-=======
             <SongTable songInput={searchTerm} />
->>>>>>> 8b489b810d48c687a294678b661f4e2a0fbae8e7
           </main>
         </div>
       </div>
