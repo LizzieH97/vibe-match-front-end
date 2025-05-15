04bfdf3a9ad0062fcbeb93abdf906ca818fb1b6f
Lotanna <tannaloamobi@gmail.com>
2025-05-16 00:57:38 +0100

    testing songtable logic

--- vibe-match/src/components/SongTable.tsx
+++ vibe-match/src/components/SongTable.tsx
@@ -7,57 +7,65 @@ import {
   TableRow,
   Paper,
 } from "@mui/material";
+import type { Song } from "../types/Song";
 import { getAllSongs } from "../utils/CallingFunctions";
+import { useState } from "react";
 
-interface SongTableProps {
+type SongTableProps = {
   songInput: string;
-}
+};
 
 const SongTable = ({ songInput }: SongTableProps) => {
-  const mockSongs = getAllSongs();
+  const [selectedGenreId, setSelectedGenreId] = useState<number | null>(null);
 
-  const filteredSongs = mockSongs.filter((song) =>
-    song.track_name.toLowerCase().includes(songInput.toLocaleLowerCase())
+  const allSongs = getAllSongs();
+
+  let filteredSongs: Song[] = [];
+
+  const targetSongMatch = allSongs.find(
+    (song: Song) => song.track_name.toLowerCase() === songInput.toLowerCase()
   );
 
-  // to prevent non filtered data from displaying the table
-  if (!songInput) {
-    return null;
+  if (targetSongMatch) {
+    setSelectedGenreId(targetSongMatch.genre.id);
   }
 
-  return (
-    <TableContainer
-      component={Paper}
-      sx={{
-        minWidth: 400,
-        width: "40%",
-        margin: "0 auto",
-      }}
-    >
-      <Table className="song-table" aria-label="table of songs">
-        <TableHead className="song-table-header">
-          <TableRow>
-            <TableCell>#</TableCell>
-            <TableCell align="center">SONG TITLE</TableCell>
-            <TableCell align="center">ARTIST</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {filteredSongs.map((song) => (
-            <TableRow key={song.id} className="song-table-row">
-              <TableCell component="th" scope="row">
-                {song.id}
-              </TableCell>
-              <TableCell align="center" component="th" scope="row">
-                {song.track_name}
-              </TableCell>
-              <TableCell align="center">{song.genre.genre}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+  filteredSongs = allSongs.filter(
+    (song: Song) => song.genre.id === selectedGenreId
   );
+
+  if (songInput && filteredSongs.length > 0) {
+    return (
+      <TableContainer
+        component={Paper}
+        sx={{
+          minWidth: 400,
+          width: "40%",
+          margin: "0 auto",
+        }}
+      >
+        <Table className="song-table" aria-label="table of songs">
+          <TableHead className="song-table-header">
+            <TableRow>
+              <TableCell align="center">SONG TITLE</TableCell>
+              <TableCell>GENRE</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredSongs.map((song) => (
+              <TableRow key={song.spotify_id} className="song-table-row">
+                <TableCell align="center" component="th" scope="row">
+                  {song.track_name}
+                </TableCell>
+                <TableCell align="center">{song.genre.genre}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+  return null;
 };
 
 export default SongTable;
