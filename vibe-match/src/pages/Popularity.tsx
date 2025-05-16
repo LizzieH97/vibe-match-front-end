5bd41206fd4c95e05ed4211a34c7f2676441b939
Lotanna <tannaloamobi@gmail.com>
2025-05-16 14:28:50 +0100

    edited styling and popularity component

--- vibe-match/src/pages/Popularity.tsx
+++ vibe-match/src/pages/Popularity.tsx
@@ -8,16 +8,18 @@ const Popularity = () => {
       <SideBar />
       <div className="main-content">
         <Header />
-        <main className="choice-grid">
-          <h2 className="subtitle">
-            Great choice! Here's some of the most popular songs currently:
-          </h2>
+        <main>
+          <div className="page-info-section">
+            <h2 className="subtitle">
+              Great choice! Here's some of the most popular songs currently:
+            </h2>
+            <a href="/" id="back-button" className="help-back">
+              Want to go back to songs?
+            </a>
+          </div>
           <div className="pop-table">
             <PopularityTable />
           </div>
-          <a href="/" id="back-button" className="help-back">
-            Want to go back to songs?
-          </a>
         </main>
       </div>
     </div>
